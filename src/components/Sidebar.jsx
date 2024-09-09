9578d25f76d8ceff124f205f23cec78e70d8342c
atharv-av <atharvv333@gmail.com>
2024-09-10 02:21:16 +0530

    added a modal for logout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,51 +13,64 @@ import {
   Settings,
   UserRound,
 } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SidebarHeader from "./SidebarHeader";
-import { deleteCookie } from "../utils/cookieHandler";
 
-const menuItems = [
-  {
-    label: "Home",
-    icon: <House />,
-    target: "/",
-    action: () => {},
-  },
-  {
-    label: "Profile",
-    icon: <UserRound />,
-    target: "/",
-    action: () => {},
-  },
-  {
-    label: "Clips",
-    icon: <Clapperboard />,
-    target: "/",
-    action: () => {},
-  },
-  {
-    label: "Messages",
-    icon: <Mail />,
-    target: "/",
-    action: () => {},
-  },
-  {
-    label: "Settings",
-    icon: <Settings />,
-    target: "/",
-    action: () => {},
-  },
-  {
-    label: "Log Out",
-    icon: <LogOut />,
-    action: () => {
-      deleteCookie("token");
-    },
-  },
-];
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+import { deleteCookie } from "../utils/cookieHandler";
+import { useState } from "react";
 
 function Sidebar() {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleOpen = () => setOpen(!open);
+
+  const menuItems = [
+    {
+      label: "Home",
+      icon: <House />,
+      target: "/",
+      action: () => {},
+    },
+    {
+      label: "Profile",
+      icon: <UserRound />,
+      target: "/",
+      action: () => {},
+    },
+    {
+      label: "Clips",
+      icon: <Clapperboard />,
+      target: "/",
+      action: () => {},
+    },
+    {
+      label: "Messages",
+      icon: <Mail />,
+      target: "/",
+      action: () => {},
+    },
+    {
+      label: "Settings",
+      icon: <Settings />,
+      target: "/",
+      action: () => {},
+    },
+    {
+      label: "Log Out",
+      icon: <LogOut />,
+      action: () => {
+        handleOpen();
+      },
+    },
+  ];
   return (
     <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
       <div className="mb-2 p-4">
@@ -79,6 +92,30 @@ function Sidebar() {
             </ListItem>
           </Link>
         ))}
+        <Dialog open={open} handler={handleOpen} className="p-6">
+          <DialogHeader>Do you want to log out?</DialogHeader>
+          <DialogFooter>
+            <Button
+              variant="text"
+              color="black"
+              onClick={handleOpen}
+              className="mr-1"
+            >
+              <span>Cancel</span>
+            </Button>
+            <Button
+              variant="gradient"
+              color="red"
+              onClick={() => {
+                deleteCookie("token");
+                handleOpen();
+                navigate("/");
+              }}
+            >
+              <span>Confirm</span>
+            </Button>
+          </DialogFooter>
+        </Dialog>
       </List>
     </Card>
   );
